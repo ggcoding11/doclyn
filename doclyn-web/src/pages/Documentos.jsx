1458668feba46d783fe28427ea5a55d77c71af0f
Gustavo de Almeida Gomes <gustavogomes1108@gmail.com>
2026-08-24 14:51:58 -0300

    #Feat: adição da sidebar

---
+++ doclyn-web/src/pages/Documentos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+const Documentos = () => {
+  return (
+    <Sidebar activeMenu={"documentos"}>
+      <div id="main">Documentos</div>
+    </Sidebar>
+  );
+};
+
+export default Documentos;
---
+++ doclyn-web/src/pages/Funcionarios.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+const Funcionarios = () => {
+  return (
+    <Sidebar activeMenu={"funcionarios"}> 
+      <div id="main">
+        Funcionarios
+      </div>
+    </Sidebar>
+  )
+}
+
+export default Funcionarios
